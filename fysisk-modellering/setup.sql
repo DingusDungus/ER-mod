136f535f617b66a0e5c2f1adb274f304fb67721d
Noah <noah@pop-os.localdomain>
2021-02-23 11:54:00 +0100

    added mysql version for user create. + commented user creation.

--- fysisk-modellering/setup.sql
+++ fysisk-modellering/setup.sql
@@ -1,14 +1,39 @@
 DROP DATABASE IF EXISTS eshop;
-DROP USER IF EXISTS 'user'@'localhost';
 
 CREATE DATABASE eshop;
+
+
+
+
+
+-- MariaDB
+/* DROP USER IF EXISTS 'user'@'localhost';
 
 CREATE USER IF NOT EXISTS 'user'@'localhost'
 	IDENTIFIED BY 'pass'
 ;
 
+-- Ge användaren alla rättigheter på alla eshop.
 GRANT ALL PRIVILEGES
     ON eshop.*
     TO 'user'@'localhost'
     WITH GRANT OPTION
+; */
+
+-- MYSQL
+/* DROP USER IF EXISTS 'user'@'%';
+
+
+CREATE USER IF NOT EXISTS 'user'@'%' -- % = godtycklig host.
+IDENTIFIED
+WITH mysql_native_password -- Only MySQL > 8.0.4 bakåtkompatibelt.
+BY 'pass'
 ;
+
+-- Ge användaren alla rättigheter på alla databaser.
+GRANT ALL PRIVILEGES
+    ON eshop.*
+    TO 'user'@'%'
+; */
+
+SHOW GRANTS FOR 'user'@'%';
